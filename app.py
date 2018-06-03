b6d4360e28794c99d03404a3aa06afb27a50dccd
SwiatekOrg <31080298+SwiatekOrg@users.noreply.github.com>
2018-06-03 21:45:23 +0200

    AddedSettingsInNav

--- app.py
+++ app.py
@@ -10,39 +10,45 @@ def index():
 
 @app.route('/login', methods=['GET','POST'])
 def login():
-    if request.method == 'POST':
-        if request.form['username'] == "test" and request.form['password'] == "haslo":
-            session['logged_in'] = True
-            flash("You were successfully logged in")
-            return render_template('index.html')
-        else:
-            session['logged_in'] = False
-            flash("You typed wrong username or password!")
-            return render_template('login.html')
-    else:
-        return render_template('login.html')
+	db = DataBase()
+	if request.method == 'POST':
+		username = request.form['username']
+		password = request.form['password']
+		if db.login_to_account(username,password):
+			session['logged_in'] = True
+			return render_template('index.html')
+		else:
+			session['logged_in'] = False
+			flash("You typed wrong username or password!")
+			return render_template('login.html')
+	else:
+		return render_template('login.html')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    db = DataBase()
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        email = request.form['email']
-        if db.add_account(username,password,email):
-            flash("You were successfully registered!")
-            return render_template('register.html')
-        else:
-            flash("That user arleady exist")
-            return render_template('register.html')
-    return render_template('register.html')
+	db = DataBase()
+	if request.method == 'POST':
+		username = request.form['username']
+		password = request.form['password']
+		email = request.form['email']
+		if db.add_account(username,password,email):
+			flash("You were successfully registered!")
+			return render_template('register.html')
+		else:
+			flash("That user arleady exist")
+			return render_template('register.html')
+	return render_template('register.html')
 
 
 @app.route('/logout',methods=['GET'])
 def logut():
-    session['logged_in'] = False
-    return redirect(url_for('index'))
+	session['logged_in'] = False
+	return redirect(url_for('index'))
+
+@app.route('/settings',methods=['GET','POST'])
+def settings():
+	return render_template('settings.html')
 
 if __name__ == '__main__':
-    app.secret_key = "jdu7x3j8e83iej7eeh8e"
-    app.run(port=5011)
+	app.secret_key = "jdu7x3j8e83iej7eeh8e"
+	app.run(port=5011)
--- database.py
+++ database.py
@@ -2,39 +2,46 @@ import sqlite3
 
 class DataBase():
 
-	def __init__(self):
-		self.conn = None
-		self.connect()
-		self.cursor = self.get_cursor()
+    def __init__(self):
+        self.conn = None
+        self.connect()
+        self.cursor = self.get_cursor()
         #self.cursor.execute('''CREATE TABLE stocks
              #(user_id int, username text, password text, email text)''')
-		self.conn.commit()
+        self.conn.commit()
 
-	def connect(self):
-		self.conn = sqlite3.connect('database1.db')
+    def connect(self):
+        self.conn = sqlite3.connect('database1.db')
 
-	def get_cursor(self):
-		return self.conn.cursor()
+    def get_cursor(self):
+        return self.conn.cursor()
 
-	def get_account(self,username):
-		self.cursor.execute("SELECT * FROM users WHERE username=\"%s\"" % username)
-		user = self.cursor.fetchone()
-		return user
+    def get_account(self,username):
+        self.cursor.execute("SELECT * FROM users WHERE username=\"%s\"" % username)
+        user = self.cursor.fetchone()
+        return user
 
-	def get_user_username(self,username):
-		self.cursor.execute("SELECT username FROM users WHERE username=\"%s\"" % username)
-		username = self.cursor.fetchone()
-		return username
+    def get_user_username(self,username):
+        self.cursor.execute("SELECT username FROM users WHERE username=\"%s\"" % username)
+        username = self.cursor.fetchone()
+        return username
 
-	def get_user_password(self,username):
-		self.cursor.execute("SELECT password FROM users WHERE username=\"%s\"" % username)
-		password = self.cursor.fetchone()
-		return password
+    def get_user_password(self,username):
+        self.cursor.execute("SELECT password FROM users WHERE username=\"%s\"" % username)
+        password = self.cursor.fetchone()
+        return password
 
-	def add_account(self,username,password,email):
-		if self.get_user_username(username) is not None:
-			return False
-		else:
-			self.cursor.execute("INSERT INTO users(username, password, email) VALUES(\"%s\", \"%s\", \"%s\")" % (username, password, email))
-			self.conn.commit()
-			return True
+    def login_to_account(self,username,password):
+        user = self.get_account(username)
+        if str(user[1]) == username and str(user[2]) == password:
+            return True
+        else:
+            return False
+
+    def add_account(self,username,password,email):
+        if self.get_user_username(username) is not None:
+            return False
+        else:
+            self.cursor.execute("INSERT INTO users(username, password, email) VALUES(\"%s\", \"%s\", \"%s\")" % (username, password, email))
+            self.conn.commit()
+            return True
